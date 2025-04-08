52cc1e0251d1eed646739ca8766dda69d7ae719c
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-04-08 10:28:35 -0600

    Agregando metodo de lectura de datos en las clases de los DAO.

--- src/DAO/CategoriaDAO.java
+++ src/DAO/CategoriaDAO.java
@@ -9,10 +9,15 @@ import Util.ConexionDB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Estudiantes
+ * 
  */
+
 public class CategoriaDAO {
     
     public void crearCategoria(Categoria categoria) throws SQLException{
@@ -30,16 +35,38 @@ public class CategoriaDAO {
     }
 }
 
-public static void main(String[] args) {
-    try {
-        CategoriaDAO dao = new CategoriaDAO();
-        Categoria c1 = new Categoria();
-        c1.setNombreCategoria("Construcción");
-        c1.setDescripcionCategoria("Herramientas de cosntrucción");
-        dao.crearCategoria(c1);
-        System.out.println("Categoría creada con éxito!");
-    } catch (SQLException e) {
-        System.err.println("Error: " + e.getMessage());
-    }
+    // Método para leer todas las categorías
+    public List<Categoria> leerTodasCategorias() throws SQLException {
+        
+        String sql = "SELECT * FROM Categorias";
+        List<Categoria> categorias = new ArrayList<>();
+    
+        try (Connection c = ConexionDB.getConnection();
+            
+            PreparedStatement stmt = c.prepareStatement(sql);
+            
+            ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Categoria categoria = new Categoria();
+                categoria.setIdCategoria(rs.getInt("id_categoria"));
+                categoria.setNombreCategoria(rs.getString("nombre_categoria"));
+                categoria.setDescripcionCategoria(rs.getString("descripcion_categoria"));
+                categorias.add(categoria);
+            }
+        }
+        return categorias;
 }
+    
+    public static void main(String[] args) throws SQLException {
+        CategoriaDAO dao = new CategoriaDAO() ;
+
+        // Leer y mostrar todas las categorías
+        List<Categoria> categorias = dao.leerTodasCategorias();
+        System.out.println("\nLista de categorías:");
+        for (Categoria cat : categorias) {
+            System.out.println("ID: " + cat.getIdCategoria()
+                    + ", Nombre: " + cat.getNombreCategoria()
+                    + ", Descripción: " + cat.getDescripcionCategoria());
+        }
+    }
 }
--- src/DAO/EmpleadoDAO.java
+++ src/DAO/EmpleadoDAO.java
@@ -11,6 +11,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Estudiantes
@@ -29,33 +32,57 @@ public class EmpleadoDAO {
             fecha_contratacion
         ) VALUES (?, ?, ?, ?, ?, ?, ?)""";
     
-    try (Connection c = ConexionDB.getConnection();
-         PreparedStatement stmt = c.prepareStatement(sql)) {
-        stmt.setString(1, empleado.getPrimerNombre());
-        stmt.setString(2, empleado.getSegundoNombre());
-        stmt.setString(3, empleado.getPrimerApellido());
-        stmt.setString(4, empleado.getSegundoApellido());
-        stmt.setString(5, empleado.getCelular());
-        stmt.setString(6, empleado.getCargo());
-        stmt.setDate(7, new java.sql.Date(empleado.getFechaContratacion().getTime()));
-        stmt.executeUpdate();
+        try (Connection c = ConexionDB.getConnection();
+             PreparedStatement stmt = c.prepareStatement(sql)) {
+            stmt.setString(1, empleado.getPrimerNombre());
+            stmt.setString(2, empleado.getSegundoNombre());
+            stmt.setString(3, empleado.getPrimerApellido());
+            stmt.setString(4, empleado.getSegundoApellido());
+            stmt.setString(5, empleado.getCelular());
+            stmt.setString(6, empleado.getCargo());
+            stmt.setDate(7, new java.sql.Date(empleado.getFechaContratacion().getTime()));
+            stmt.executeUpdate();
+        }
     }
+    
+    public List<Empleado> leerTodosEmpleados() throws SQLException {
+        String sql = "SELECT * FROM Empleados";
+        List<Empleado> empleados = new ArrayList<>();
+
+        try (Connection c = ConexionDB.getConnection();
+             PreparedStatement stmt = c.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Empleado empleado = new Empleado();
+                empleado.setIdEmpleado(rs.getInt("id_empleado"));
+                empleado.setPrimerNombre(rs.getString("primer_nombre"));
+                empleado.setSegundoNombre(rs.getString("segundo_nombre"));
+                empleado.setPrimerApellido(rs.getString("primer_apellido"));
+                empleado.setSegundoApellido(rs.getString("segundo_apellido"));
+                empleado.setCelular(rs.getString("celular"));
+                empleado.setCargo(rs.getString("cargo"));
+                empleado.setFechaContratacion(rs.getDate("fecha_contratacion"));
+                empleados.add(empleado);
+            }
+        }
+        return empleados;
     }
+    
     public static void main(String[] args) {
-        try {
-        EmpleadoDAO dao = new EmpleadoDAO();
-        Empleado e1 = new Empleado();
-        e1.setPrimerNombre("María");
-        e1.setSegundoNombre("Luisa");
-        e1.setPrimerApellido("Rodríguez");
-        e1.setSegundoApellido("López");
-        e1.setCelular("87654321");
-        e1.setCargo("Vendedor");
-        e1.setFechaContratacion(new Date());
-        dao.crearEmpleado(e1);
-        System.out.println("Empleado creado con éxito!");
+       try {
+            EmpleadoDAO dao = new EmpleadoDAO();
+            List<Empleado> empleados = dao.leerTodosEmpleados();
+            System.out.println("Lista de empleados:");
+            for (Empleado emp : empleados) {
+                System.out.println("ID: " + emp.getIdEmpleado() + 
+                                 ", Nombre: " + emp.getPrimerNombre() + " " + emp.getSegundoNombre() + 
+                                 " " + emp.getPrimerApellido() + " " + emp.getSegundoApellido() + 
+                                 ", Celular: " + emp.getCelular() + 
+                                 ", Cargo: " + emp.getCargo() + 
+                                 ", Fecha Contratación: " + emp.getFechaContratacion());
+            }
         } catch (SQLException e) {
-        System.err.println("Error: " + e.getMessage());
-    }
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
